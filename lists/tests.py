7e0394a5e43d72886ad06cd514e372f51ebbaa57
Daniel Stein <eziril@gmail.com>
2020-05-14 23:23:29 -0600

    Generating a form template

--- lists/tests.py
+++ lists/tests.py
@@ -12,6 +12,7 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)

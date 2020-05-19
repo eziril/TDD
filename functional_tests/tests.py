ee43e6bce0fc779d0bf8f5f2ebb8cbe3dc54b7cd
Daniel Stein <eziril@gmail.com>
2020-05-19 17:25:01 -0600

    stagin missed folders

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,14 @@
+'''Live Server Unit test'''
+
+import time
+
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         WINDOW_SIZE = "1920,1080"
@@ -25,7 +29,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -59,5 +63,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
 
-if __name__ == '__main__':
-    unittest.main()
